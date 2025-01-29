197f05399add41bb5c12d6911bebc76ad9b7d658
Stanex96 <coqalexandre.pro@gmail.com>
2025-01-30 00:12:50 +0100

    Intro section with dropdown navigation
    Intro section with dropdown navigation

---
+++ Js/main.js
@@ -0,0 +1,36 @@
+// ***** Elements from HTML file *****
+const features     = document.querySelector('#features');
+const company      = document.querySelector('#company');
+const featuresMenu = document.querySelector('#featuresMenu');
+const companyMenu  = document.querySelector('#companyMenu');
+
+// ***** Arrows from HTML file *****
+const firstArrowDown  = document.querySelector('#firstArrowDown');
+const firstArrowUp    = document.querySelector('#firstArrowUp');
+const secondArrowDown = document.querySelector('#secondArrowDown');
+const secondArrowUp   = document.querySelector('#secondArrowUp');
+
+// ***** Event menus *****
+features.addEventListener('click', () => {
+    if (featuresMenu.style.display === 'none' || featuresMenu.style.display === '') {
+        featuresMenu.style.display      = 'flex';
+        firstArrowDown.style.display    = 'none';
+        firstArrowUp.style.display      = 'inline';
+    } else {
+        featuresMenu.style.display      = 'none';
+        firstArrowDown.style.display    = 'inline';
+        firstArrowUp.style.display      = 'none';
+    }
+});
+
+company.addEventListener('click', () => {
+    if (companyMenu.style.display === 'none' || companyMenu.style.display === '') {
+        companyMenu.style.display        = 'flex';
+        secondArrowDown.style.display    = 'none';
+        secondArrowUp.style.display      = 'inline';
+    } else {
+        companyMenu.style.display        = 'none';
+        secondArrowDown.style.display    = 'inline';
+        secondArrowUp.style.display      = 'none';
+    }
+});
